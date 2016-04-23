e9bea7601ce1e0ae2e298db1fc685f8cbe38a333
cirograciapons <cirograciapons@gmail.com>
2016-04-23 21:03:21 +0200

    add regression patch mnist
    correct mnist error
    clone mnist as a template for regression

--- examples/regression/regression_experiments.m
+++ examples/regression/regression_experiments.m
@@ -1,23 +1,23 @@
 %% Experiment with the cnn_mnist_fc_bnorm
 
-[net_bn, info_bn] = cnn_mnist(...
+[net_bn, info_bn] = regression(...
   'expDir', 'data/mnist-bnorm', 'batchNormalization', true);
 
-[net_fc, info_fc] = cnn_mnist(...
+[net_fc, info_fc] = regression(...
   'expDir', 'data/mnist-baseline', 'batchNormalization', false);
 
 figure(1) ; clf ;
 subplot(1,2,1) ;
-semilogy(cat(1,info_fc.val(:).objective), 'o-') ; hold all ;
-semilogy(cat(1,info_bn.val(:).objective)', '+--') ;
+semilogy(info_fc.val.objective', 'o-') ; hold all ;
+semilogy(info_bn.val.objective', '+--') ;
 xlabel('Training samples [x 10^3]'); ylabel('energy') ;
 grid on ;
 h=legend('BSLN', 'BNORM') ;
 set(h,'color','none');
 title('objective') ;
 subplot(1,2,2) ;
-plot(cat(1,info_fc.val.top1err)', 'o-') ; hold all ;
-plot(cat(1,info_bn.val.top1err)', '+--') ;
+plot(info_fc.val.error', 'o-') ; hold all ;
+plot(info_bn.val.error', '+--') ;
 h=legend('BSLN-val','BSLN-val-5','BNORM-val','BNORM-val-5') ;
 grid on ;
 xlabel('Training samples [x 10^3]'); ylabel('error') ;
